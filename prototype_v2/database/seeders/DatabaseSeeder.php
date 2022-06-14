76ead8ed4430b1685bf3785ab3773b4370fac8ce
ouailagzenay <ouailagzenay@gmail.com>
2022-06-14 20:43:50 +0100

    ajouter admin dans dataseeder

--- prototype_v2/database/seeders/DatabaseSeeder.php
+++ prototype_v2/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
           
             CategorieSeeder::class,
             MaterailSeeder::class,
+            AdminSeeder::class,
            
         ]);
     }
